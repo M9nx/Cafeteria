3b28d2a564b315ea00eecc51ac483fbcfdeeefeb
Basha Wahed <bashawahed573@gmail.com>
2026-09-01 19:12:24 +0300

    feat(admin): add admin routes

---
+++ routes/admin.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+use Cafeteria\Controllers\Admin\CategoryController;
+use Cafeteria\Controllers\Admin\UserController;
+
+$router->get(
+    '/admin/categories',
+    [CategoryController::class, 'index'],
+    [$adminMiddleware]
+);
+
+$router->get(
+    '/admin/categories/create',
+    [CategoryController::class, 'create'],
+    [$adminMiddleware]
+);
+
+$router->post(
+    '/admin/categories',
+    [CategoryController::class, 'store'],
+    [$adminMiddleware]
+);
+
+$router->get(
+    '/admin/categories/{id}/edit',
+    [CategoryController::class, 'edit'],
+    [$adminMiddleware]
+);
+
+$router->post(
+    '/admin/categories/{id}/update',
+    [CategoryController::class, 'update'],
+    [$adminMiddleware]
+);
+
+$router->post(
+    '/admin/categories/{id}/deactivate',
+    [CategoryController::class, 'deactivate'],
+    [$adminMiddleware]
+);
+
+
+$router->get(
+    '/admin/users',
+    [UserController::class, 'index'],
+    [$adminMiddleware]
+);
+
+$router->get(
+    '/admin/users/create',
+    [UserController::class, 'create'],
+    [$adminMiddleware]
+);
+
+$router->post(
+    '/admin/users',
+    [UserController::class, 'store'],
+    [$adminMiddleware]
+);
+
+$router->get(
+    '/admin/users/{id}/edit',
+    [UserController::class, 'edit'],
+    [$adminMiddleware]
+);
+
+$router->post(
+    '/admin/users/{id}/update',
+    [UserController::class, 'update'],
+    [$adminMiddleware]
+);
+
+$router->post(
+    '/admin/users/{id}/deactivate',
+    [UserController::class, 'deactivate'],
+    [$adminMiddleware]
+);
