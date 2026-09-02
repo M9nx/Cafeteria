98acaa5a5781103e71b66c78aa0d74ce5be66f37
salma <salmafathy274@gmail.com>
2026-09-02 07:24:15 +0300

    feat(orders): add authenticated order routes

---
+++ routes/orders.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Cafeteria\Controllers\User\CatalogController;
+use Cafeteria\Controllers\User\OrderController;
+
+$router->get(
+    '/',
+    [CatalogController::class, 'index'],
+    [$authMiddleware]
+);
+
+$router->get(
+    '/orders/create',
+    [OrderController::class, 'create'],
+    [$authMiddleware]
+);
+
+$router->post(
+    '/orders',
+    [OrderController::class, 'store'],
+    [$authMiddleware]
+);
